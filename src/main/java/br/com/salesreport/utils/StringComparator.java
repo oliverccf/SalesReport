5997aa65ee61a66aee1778f0c4a8c0f873ab338d
oliveirafilho <oliverccf@gmail.com>
2020-11-09 21:04:07 -0300

    Nome de classe modificada

--- src/main/java/br/com/salesreport/utils/StringComparator.java
+++ src/main/java/br/com/salesreport/utils/StringComparator.java
@@ -1,6 +1,6 @@
 package br.com.salesreport.utils;
 
-public class RecordFieldValidator {
+public class StringCompare {
 
     public static Boolean compareIfEqual(String firstValue, String secondValue) {
         return firstValue.equals(secondValue);
